ff8a11215515415dfd045d89dcd09c67800a51e0
kaszlarz <kaszlarz@op.pl>
2022-07-08 08:56:41 +0200

    dodanie zapisu kilku parametrow proca i ram do csv

--- main.cpp
+++ main.cpp
@@ -2,20 +2,30 @@
 #include <stdio.h>
 #include <fstream>
 #include <chrono>
+#include <linux/kernel.h>
+#include <sys/sysinfo.h>
+
 //#include <thread>
 using namespace std;
 double current,previous = 0.0;
-double interval = 1.0;
+double interval = 1.0; //0.05 50ms
 
 
-int main()
+int main(int argc, char** argv)
 {
-    
+    const double mb=1024*1024;
+    const float load_avg_sysinfo_scale = 2<<16;
+
+    struct sysinfo s;
+    //sysinfo (&s);
+
+
+
     fstream plikZapis;
     plikZapis.open("plik.csv",ios::out | ios::trunc);
             if (plikZapis.is_open())
             {
-                plikZapis << "TIME" << "\n";
+                plikZapis << "TIME,Load1,Load5,Load15,FreeRAM" << "\n";
                 plikZapis.close();
             } else return -1;
     
@@ -32,11 +42,12 @@ int main()
         if (current - previous >= interval)
         {
             //wykonuj operacje co interval
+             sysinfo (&s);
             printf("\a");
             plikZapis.open("plik.csv",ios::app);
             if (plikZapis.is_open())
             {
-                plikZapis << current << "\n";
+                plikZapis << current << ","<< s.loads[0]/load_avg_sysinfo_scale << ","<< s.loads[1]/load_avg_sysinfo_scale << "," << s.loads[2]/load_avg_sysinfo_scale << "," << (s.freeram/mb) <<"\n";
                 plikZapis.close();
             }
             
